eff683840b42ed08ffa525ce274c107ed4ae4375
Sean Dolphin <Sean.Dolphin@kpcompass.com>
2015-05-17 13:23:50 -0700

    updated readme to hold a license badge

--- getUserID.go
+++ getUserID.go
@@ -2,9 +2,11 @@ package jwtcontext
 
 import "golang.org/x/net/context"
 
-//GetClaims gets the claims associated with a token
+//GetUserID gets the userID associated with a token
 func GetUserID(ctx context.Context) string {
 	jwt := jc(ctx)
-
+	if !jwt.Valid {
+		return ""
+	}
 	return jwt.Claims["sub"].(string)
 }
--- getUserID_test.go
+++ getUserID_test.go
@@ -14,6 +14,23 @@ var _ = Describe("GetUserID", func() {
 	It("should panic when no context is added", func() {
 		Expect(func() { GetUserID(ctx) }).To(Panic())
 	})
+
+	It("should return blank when the token is not valid", func() {
+
+		req, err := http.NewRequest("GET", "/", &bytes.Buffer{})
+		Expect(err).NotTo(HaveOccurred())
+
+		// req.Header.Set(http.CanonicalHeaderKey("Authorization"), fmt.Sprintf("Bearer %s", "badtoken"))
+
+		req = authorizeRequest(req, "badtoken")
+		token := req.Header.Get(http.CanonicalHeaderKey("Authorization"))
+		req.Header.Set(http.CanonicalHeaderKey("Authorization"), token+"a")
+		ctx := jwtTestContext("badtoken")(ctx, req)
+
+		Expect(GetUserID(ctx)).To(Equal(""))
+
+	})
+
 	It("should return the id of the user", func() {
 		for _, email := range emails {
 			req, err := http.NewRequest("GET", "/", &bytes.Buffer{})
